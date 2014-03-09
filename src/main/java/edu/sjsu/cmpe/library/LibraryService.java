42cba78db8b2368390b3a3ee9c2f71708e7791e0
akhilmsreenath <akhilmsreenath@gmail.com>
2014-03-08 21:40:41 -0800

    library

--- src/main/java/edu/sjsu/cmpe/library/LibraryService.java
+++ src/main/java/edu/sjsu/cmpe/library/LibraryService.java
@@ -7,15 +7,20 @@ import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
 
 import edu.sjsu.cmpe.library.api.resources.BookResource;
+import edu.sjsu.cmpe.library.api.resources.ReviewResource;
 import edu.sjsu.cmpe.library.api.resources.RootResource;
 import edu.sjsu.cmpe.library.config.LibraryServiceConfiguration;
 import edu.sjsu.cmpe.library.domain.Book;
+import edu.sjsu.cmpe.library.domain.Review;
 import edu.sjsu.cmpe.library.repository.BookRepository;
 import edu.sjsu.cmpe.library.repository.BookRepositoryInterface;
+import edu.sjsu.cmpe.library.repository.ReviewRepository;
+import edu.sjsu.cmpe.library.repository.ReviewRepositoryInterface;
 
 public class LibraryService extends Service<LibraryServiceConfiguration> {
 
     public static void main(String[] args) throws Exception {
+    	System.out.println("Inside Main");
 	new LibraryService().run(args);
     }
 
@@ -33,6 +38,12 @@ public class LibraryService extends Service<LibraryServiceConfiguration> {
 	BookRepositoryInterface bookRepository = new BookRepository(
 		new ConcurrentHashMap<Long, Book>());
 	environment.addResource(new BookResource(bookRepository));
+	
+	ReviewRepositoryInterface reviewRepository = new ReviewRepository(
+			new ConcurrentHashMap<Integer, Review>());
+	environment.addResource(new ReviewResource(reviewRepository));
+	//ReviewRepositoryInterface reviewRepository1 = new ReviewRepository(
+		
 	/** Add new resources here */
     }
 }
